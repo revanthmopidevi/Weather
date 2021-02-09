c7ab5e667920b42645d08315b7793724bb95c2e1
revanthmopidevi <venkatasai.revanthmopidevi@lntinfotech.com>
2021-02-09 19:22:36 +0530

    port

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const { traceDeprecation } = require('process')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -69,6 +70,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000.")
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`)
 })
